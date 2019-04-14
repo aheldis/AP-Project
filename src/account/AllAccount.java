6a2c15c50586ae6dfe1abdc27e6bbfede59a09ef
zahrayousefijamarani <45602698+zahrayousefijamarani@users.noreply.github.com>
2019-04-15 00:41:44 +0430

    add src file

---
+++ src/account/AllAccount.java
@@ -0,0 +1,31 @@
+package account;
+
+import java.util.ArrayList;
+
+public class AllAccount {
+
+    public static ArrayList<Account> accounts;
+
+    public static void addToAccounts(Account account){
+
+    }
+    public static void getAccountsArrayList(){
+
+    }
+    public static Account getAccountByName(String userName){
+
+    }
+    public static boolean userNameHaveBeenExist(String userName){
+
+    }
+    public static void login(String userName){
+
+    }
+    public static void showLeaderBoard(){
+
+    }
+    public static void helpOfAccount(){
+
+    }
+
+}
---
+++ src/account/Collection.java
@@ -0,0 +1,67 @@
+package account;
+
+
+public class Collection {
+
+    private ArrayList<Spell> spells;
+    private ArrayList<Hero> heros;
+    private ArrayList<Minion> miniions;
+    private Arraylist<Item>items;
+
+
+    public void showCardsAndItems(){
+
+    }
+    public void searchCardName(String cardName){
+
+    }
+    public void save(){
+
+    }
+    private boolean deckHaveBeenExist(String deckName){
+
+    }
+    public void createDeck(String deckName){
+
+    }
+    public int deleteDeck(String deckName){
+
+    }
+    private int numberOfCardsForThisDeck(String deckName){
+
+    }
+    private int numberOfHerosForThisDeck(String deckName){
+
+    }
+    private boolean heroHaveBeenExistInThisDeck(String deckName,int heroId){
+
+    }
+    private boolean cardHaveBeenExistInThisDeck(String deckName,int cardId){
+
+    }
+    public void addCard(int cardId,String deckName){
+
+    }
+    public void addHero(int heroId,String deckName){
+
+    }
+    public void removeCard(int cardId,String deckName){
+
+    }
+    public void removeHero(int heroId,String deckName){
+
+    }
+    public void validateDeck(String deckName){
+
+    }
+    public void showAlldecks(){
+
+    }
+    public void showThisDeck(String deckName){
+
+    }
+    public void helpOfCollection(){
+
+    }
+
+}
