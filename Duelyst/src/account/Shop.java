225bf0fec5d9269f9afef71b142e690bf59678c2
Saba H <Savaw97@gmail.com>
2019-04-16 23:13:32 +0430

    add Shop class

--- Duelyst/src/account/Shop.java
+++ Duelyst/src/account/Shop.java
@@ -1,4 +1,42 @@
 package account;
 
 public class Shop {
+    private ArrayList<Card> cards;
+    private ArrayList<Item> items;
+
+    private boolean cardOrItemExist(String name){
+
+    }
+
+    private CardId getExistingCardId(String name){
+
+    }
+
+    private CardId getNewCardId(Card card){
+
+    }
+
+    public void addCard(Card card){
+
+    }
+
+    public void addItem(Item item){
+
+    }
+
+    public int search(String name){
+
+    }
+
+    public Card buy(Account account, String name){
+
+    }
+
+    public void sell(Account account, CardId cardId){
+
+    }
+
+    public static void help(){
+
+    }
 }
