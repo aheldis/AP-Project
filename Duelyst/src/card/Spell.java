491ced0f7bbce5a9b29b0c90da6022b482ccaaf7
zahrayousefijamarani <z.y.j.1379@gmail.com>
2019-04-19 00:20:50 +0430

    add cardId class

---
+++ Duelyst/src/card/Spell.java
@@ -0,0 +1,69 @@
+package card;
+
+import java.util.ArrayList;
+
+public class Spell extends Card {
+    private int playerNameByNumber;//{player1 or 2}
+    private int numberOfAttack;
+    private int turn;
+    private String description;
+    private ArrayList<Square> target;
+    private ArrayList<Buff> buffs;
+    private ArrayList<Integer> turnOfBuffs;
+    private int cost;
+    private int mp;
+    private int apChanges;
+    private int turnForApChanges;
+    private int hpChanges;
+    private int turnForHpChanges;
+
+
+    public void Spell(int cost,int mp,int apChanges,int turnForApChanges,int hpChanges
+            ,int turnForHpChanges, String description,ArrayList<Buff> buffs){
+        this.apChanges=apChanges;
+        this.cost=cost;
+        this.mp=mp;
+        this.turnForApChanges=turnForApChanges;
+        this.hpChanges=hpChanges;
+        this.description=description;
+        this.turnForApChanges=turnForHpChanges;
+        this.buffs=buffs;
+    }
+
+    public int getPlayerNameByNumber(){
+        return playerNameByNumber;
+    }
+
+    public int getNumberOfAttack(){
+        return numberOfAttack;
+    }
+
+    public int getTurn(){
+        return turn;
+    }
+    public ArrayList<Square> getTarget(){
+        return target;
+    }
+    public int getMp(){
+        return mp;
+    }
+    public int getCost(){
+        return cost;
+    }
+
+
+    public String  getDescription(){return description;}
+
+    public void printdesc(){
+        System.out.println(description);//todo sout
+    }
+
+    public ArrayList<Square> getTargets(){}
+
+    public void setTarget(Square square){}
+
+    public void affect(){
+
+    }
+
+}
