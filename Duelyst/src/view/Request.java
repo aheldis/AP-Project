57045af4d23038d46a692ef8deae466bb4edddd8
Saba H <Savaw97@gmail.com>
2019-04-20 02:04:27 +0430

    changes in request shop

--- Duelyst/src/view/Request.java
+++ Duelyst/src/view/Request.java
@@ -78,22 +78,21 @@ public class Request {
 
         }
         else if (state.equals("shop")) {
-            //todo eXiT is also true, search khali ke cardID nadare qalate :)))
-            if (command.equals("exit"))
+            if (command.toLowerCase().matches("exit"))
                 return RequestType.SHOP_EXIT;
-            else if (command.equals("show collection"))
+            else if (command.toLowerCase().matches("show collection"))
                 return RequestType.SHOP_SHOW_COLLECTION;
-            else if (command.substring(0, 17).equals("search collection"))
+            else if (command.toLowerCase().matches("search collection \\w+"))
                 return RequestType.SHOP_SEARCH_COLLECTION_CARD;
-            else if (command.substring(0, 6).equals("search"))
+            else if (command.toLowerCase().matches("search \\w+"))
                 return RequestType.SHOP_SERACH_CARD;
-            else if(command.substring(0, 3).equals("buy"))
+            else if(command.toLowerCase().matches("buy \\w+"))
                 return RequestType.SHOP_BUY;
-            else if(command.substring(0, 4).equals("sell"))
+            else if(command.toLowerCase().matches("sell \\w+"))
                 return RequestType.SHOP_SELL;
-            else if(command.equals("show"))
+            else if(command.toLowerCase().matches("show"))
                 return RequestType.SHOP_SHOW;
-            else if(command.equals("help"))
+            else if(command.toLowerCase().matches("help"))
                 return  RequestType.SHOP_HELP;
         }
         else if (state.equals("battle")) {
