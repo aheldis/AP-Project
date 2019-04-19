ec5095f34e3440345f2f4c4b51bb2ee0a72780da
zahrayousefijamarani <z.y.j.1379@gmail.com>
2019-04-19 22:03:51 +0430

    request getType()

--- Duelyst/src/view/Request.java
+++ Duelyst/src/view/Request.java
@@ -1,18 +1,61 @@
 package view;
 
+import IDK.RequestType;
+
 import java.util.Scanner;
 
 
 /**
  * mikhay esmesho bezarim Scanner (to javaDoc neveshtam ke
- *  sabz she :o )
+ * sabz she :o )
  */
 
 public class Request {
     private Scanner scanner = new Scanner(System.in);
     private String command;
+    private String state = "mainMenu";//menu or collection or shop or battle
 
     public void getNewCommand() {
         this.command = scanner.nextLine();
+    }
+
+    public boolean isValid() {
+        RequestType type = getType();
+
+    }
+
+    public RequestType getType() {
+        if (state.equals("mainMenu"))
+            switch (command.toLowerCase()) {
+                case "enter collection":
+                    return RequestType.MENU_ENTER_COLLECTION;
+                case "enter shop":
+                    return RequestType.MENU_ENTER_SHOP;
+                case "enter battle":
+                    return RequestType.MENU_ENTER_BATTLE;
+                case "enter help":
+                    return RequestType.MENU_ENTER_HELP;
+                case "enter exit":
+                    return RequestType.MENU_ENTER_EXIT;
+            }
+        else if (state.equals("collection"))
+            switch (command) {
+                case "exit":
+                    return RequestType.COLLECTION_EXIT;
+                case "show":
+                    return RequestType.COLLECTION_SHOW;
+                case "save":
+                    return RequestType.COLLECTION_SAVE;
+                case "help:":
+                    return RequestType.COLLECTION_HELP;
+            }
+        else if (state.equals("shop"))
+            switch (command) {
+
+            }
+        else if (state.equals("battle"))
+            switch (command) {
+
+            }
     }
 }
