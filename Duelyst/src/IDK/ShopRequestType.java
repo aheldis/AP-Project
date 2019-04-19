eb25cf3b966c6a1b2973fdef187792a5213cbf1f
zahrayousefijamarani <z.y.j.1379@gmail.com>
2019-04-19 20:26:55 +0430

    ADD 3 KINDS OF REQUEST

---
+++ Duelyst/src/IDK/ShopRequestType.java
@@ -0,0 +1,12 @@
+package IDK;
+
+public enum  ShopRequestType {
+    SHOP_EXIT,
+    SHOP_SHOW_COLLECTION,
+    SHOP_SERACH_CARD,
+    SHOP_SEARCH_COLLECTION_CARD,
+    SHOP_BUY,
+    SHOP_SELL,
+    SHOP_SHOW,
+    SHOP_HELP;
+}
