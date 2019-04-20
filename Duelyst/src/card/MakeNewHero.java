70ba55005beb70c629518ca93551a6e4373a59c1
Saba H <Savaw97@gmail.com>
2019-04-20 13:01:46 +0430

    add hero cards

--- Duelyst/src/card/MakeNewHero.java
+++ Duelyst/src/card/MakeNewHero.java
@@ -6,29 +6,34 @@ import java.util.Scanner;
 public class MakeNewHero {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        try {
-            FileReader fileReader = new FileReader("../CardsFile/Hero/template");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = null;
-            System.out.println("name: ");
-            String input = scanner.nextLine();
-            File file = new File("../CardsFile/Hero/" + input);
-            FileWriter fileWriter = new FileWriter(file);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-                input = scanner.nextLine();
+        while (true) {
+            try {
 
-                bufferedWriter.write(line);
-                bufferedWriter.write(" " + input);
-                bufferedWriter.newLine();
+                FileReader fileReader = new FileReader("../CardsFile/Hero/TEMPLATE");
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                String line = null;
+                System.out.println("name: ");
+                String input = scanner.nextLine();
+                if (input.equals("EXIT"))
+                    break;
+                File file = new File("../CardsFile/Hero/" + input);
+                FileWriter fileWriter = new FileWriter(file);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                    input = scanner.nextLine();
+
+                    bufferedWriter.write(line);
+                    bufferedWriter.write(" " + input);
+                    bufferedWriter.newLine();
+                }
+                bufferedWriter.close();
+
+            } catch (FileNotFoundException e) {
+                System.out.println("file not found");
+            } catch (Exception e) {
+                System.out.println("other error");
             }
-            bufferedWriter.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("file not found");
-        } catch (Exception e) {
-            System.out.println("other error");
         }
     }
 }
