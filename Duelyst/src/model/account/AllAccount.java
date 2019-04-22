84560e11c2734bd57913f299139da5d9c8133b6a
Saba H <Savaw97@gmail.com>
2019-04-22 13:16:12 +0430

    changes in all account

--- Duelyst/src/model/account/AllAccount.java
+++ Duelyst/src/model/account/AllAccount.java
@@ -7,21 +7,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class AllAccount {
+    public static AllAccount singleInstance = null;
+    public AccountView accountView = AccountView.getInstance();
+    public ArrayList<Account> accounts;
 
-    //inam singleton bashe? :-?
+    public static AllAccount getInstance() {
+        if (singleInstance == null)
+            singleInstance = new Shop();
+        return singleInstance;
+    }
 
-    public static AccountView accountView = AccountView.getInstance();
-    public static ArrayList<Account> accounts;
-
-    public static void addToAccounts(Account account) {
+    public void addToAccounts(Account account) {
         accounts.add(account);
     }
 
-    public static ArrayList<Account> getAccountsArrayList() {
+    public ArrayList<Account> getAccountsArrayList() {
         return accounts;
     }
 
-    public static Account getAccountByName(String userName) {
+    public Account getAccountByName(String userName) {
         for (Account account : accounts) {
             if (account.getUserName().equals(userName))
                 return account;
@@ -29,7 +33,7 @@ public class AllAccount {
         return null;
     }
 
-    public static boolean userNameHaveBeenExist(String userName) {
+    public boolean userNameHaveBeenExist(String userName) {
         for (Account account : accounts) {
             if (account.getUserName().equals(userName)) {
                 return true;
@@ -38,7 +42,7 @@ public class AllAccount {
         return false;
     }
 
-    public static boolean passwordMatcher(String userName, String password) {
+    public boolean passwordMatcher(String userName, String password) {
         Account account = getAccountByName(userName);
         if (account == null)
             return false;
@@ -47,7 +51,7 @@ public class AllAccount {
         return false;
     }
 
-    public static void login(String userName, String password) { //todo ina bayad bere to conroller o ina :-?
+    public void login(String userName, String password) { //todo ina bayad bere to conroller o ina :-?
         //(kheyr)todo controller miyad inja donbalet :))
         Account account = getAccountByName(userName);
         if (account == null) {//todo hamishe vojod dare chon to controller check mikonim
@@ -65,18 +69,18 @@ public class AllAccount {
 
     }
 
-    public static void showLeaderBoard() {
+    public void showLeaderBoard() {
         Collections.sort(accounts);
         for (int i = 0; i < accounts.size(); i++) {
             accountView.viewAccount(i + 1, accounts.get(i).getUserName(), accounts.get(i).getWins());
         }
     }
 
-    public static void helpOfAccount() {
+    public void helpOfAccount() {
         accountView.viewHelpOfAccount();
     }
 
-    public static void createAccount(String userName, String password) {
+    public void createAccount(String userName, String password) {
 //        if (userNameHaveBeenExist(userName)) {
 //            ErrorType error = ErrorType.USER_NAME_ALREADY_EXIST;
 //            accountView.printError(error);
