5f632e0b01375c18f418876689fa5c2b973fb2b3
zahrayousefijamarani <z.y.j.1379@gmail.com>
2019-04-23 15:30:52 +0430

    delete makeNewHero

--- Duelyst/src/model/battle/Hand.java
+++ Duelyst/src/model/battle/Hand.java
@@ -6,11 +6,15 @@ import model.card.*;
 import java.util.ArrayList;
 
 public abstract class Hand {
-    private ArrayList<Card> cards;
-    private ArrayList<Item> items;
+    protected ArrayList<Card> cards;
+    protected ArrayList<Item> items;
+
+    public void setCards(Deck deck ) {//set cards for start of game after shuttle cards in deck
+       this.cards= new ArrayList<>(deck.getCardsOfDeck().subList(0,4));
+
+    }
 
     private void addToCardsOfHand(Card card) {
-
     }
 
     private void removeFromCardsOfHand(Card card) {
@@ -26,6 +30,7 @@ public abstract class Hand {
     }
 
     public abstract void addACardOrItemFromDeck();
+    public abstract void removeUsedCardOrItem();
 
     public abstract void chooseACardOrItem();
 
--- Duelyst/src/model/battle/NormalHand.java
+++ Duelyst/src/model/battle/NormalHand.java
@@ -2,10 +2,16 @@ package model.battle;
 
 public class NormalHand extends Hand {
     public void addACardOrItemFromDeck() {
+        if(cards.size()+items.size()<5){
 
+        }
     }
 
     public void chooseACardOrItem() {
+
+    }
+
+    public void removeUsedCardOrItem(){
 
     }
 }
